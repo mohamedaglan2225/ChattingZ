527d3993f01fa3f8b71913bf4dc650d3e6f9dab0
Mohamed Aglan <62311156+mohamedaglan2225@users.noreply.github.com>
2023-10-31 17:06:06 +0200

    Initial commit

---
+++ ChattingZ/ChattingZ.h
@@ -0,0 +1,18 @@
+//
+//  ChattingZ.h
+//  ChattingZ
+//
+//  Created by Mohamed Aglan on 10/31/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ChattingZ.
+FOUNDATION_EXPORT double ChattingZVersionNumber;
+
+//! Project version string for ChattingZ.
+FOUNDATION_EXPORT const unsigned char ChattingZVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ChattingZ/PublicHeader.h>
+
+
---
+++ Podfile
@@ -0,0 +1,15 @@
+# Uncomment the next line to define a global platform for your project
+ platform :ios, '13.0'
+
+target 'ChattingZ' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for ChattingZ
+
+pod 'Alamofire', '~> 5.2'
+pod 'Kingfisher', '~> 6.0'
+pod 'Socket.IO-Client-Swift', '~> 15.2.0'
+
+
+end
